c782b551de975dcc70303a8c1b0c2247961e0076
Derek Eskens <snekse@gmail.com>
2025-12-04 21:05:17 -0600

    Hands off AI agent driven solution

--- day-01/src/test/groovy/Day01Spec.groovy
+++ day-01/src/test/groovy/Day01Spec.groovy
@@ -3,16 +3,65 @@ import spock.lang.Specification
  * https://adventofcode.com/2025/day/1
  */
 class Safe {
-    // TODO: Implement this
+    int currentPosition = 50
+
+    void rotate(String instruction) {
+        String direction = instruction.substring(0, 1)
+        int distance = instruction.substring(1).toInteger()
+
+        if (direction == 'R') {
+            currentPosition = (currentPosition + distance) % 100
+        } else if (direction == 'L') {
+            currentPosition = (currentPosition - distance) % 100
+            if (currentPosition < 0) {
+                currentPosition += 100
+            }
+        }
+    }
 }
 
 class Day01Spec extends Specification {
     def safe = new Safe()
 
-    // TODO: Write tests to solve the Day 01, Part 1 puzzle in README
-    //      Use the input.txt file for the test data
-    //      Create one test named "Solve" that solves the puzzle
-    //      The Solve might need to `println` the answer
-    //      Create additional helper methods and tests as needed
-    
+    def "rotate updates position correctly"() {
+        given:
+        safe.currentPosition = startPos
+
+        when:
+        safe.rotate(instruction)
+
+        then:
+        safe.currentPosition == endPos
+
+        where:
+        startPos | instruction | endPos
+        50       | "L68"       | 82
+        82       | "L30"       | 52
+        52       | "R48"       | 0
+        0        | "L5"        | 95
+        95       | "R60"       | 55
+        55       | "L55"       | 0
+        0        | "L1"        | 99
+        99       | "L99"       | 0
+        0        | "R14"       | 14
+        14       | "L82"       | 32
+    }
+
+    def "Solve"() {
+        given:
+        def input = new File("src/test/resources/input.txt").readLines()
+        int zeroCount = 0
+
+        when:
+        input.each { instruction ->
+            safe.rotate(instruction)
+            if (safe.currentPosition == 0) {
+                zeroCount++
+            }
+        }
+
+        then:
+        println "Password: $zeroCount"
+        zeroCount >= 0 // minimal assertion
+    }
 }
---
+++ day-01/src/test/resources/prompt.md
@@ -0,0 +1,7 @@
+There is a puzzle to solve in this module's README.md file.
+
+Create a plan to solve the puzzle, then implement it.
+
+You are only allowed to edit the Spock Specification file in this module.  You can create any classes you need within that file.
+
+There is an `input.txt` file in this module's `src/test/resources` directory.  It contains the puzzle input.
