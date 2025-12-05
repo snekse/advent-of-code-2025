95c51f78319fa0290a7cc01349ad1ab38d2ff8ad
Derek Eskens <snekse@gmail.com>
2025-12-05 09:00:25 -0600

    Setup Days 1 thru 12

---
+++ day-04/src/test/groovy/Day04Spec.groovy
@@ -0,0 +1,8 @@
+import spock.lang.Specification
+
+class Day04Spec extends Specification {
+    def "Solve"() {
+        expect:
+        true
+    }
+}
---
+++ day-09/src/test/groovy/Day09Spec.groovy
@@ -0,0 +1,8 @@
+import spock.lang.Specification
+
+class Day09Spec extends Specification {
+    def "Solve"() {
+        expect:
+        true
+    }
+}
