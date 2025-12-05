2e173d8a617ec0576abea3a22f144ca34b017ea8
Derek Eskens <snekse@gmail.com>
2025-12-04 20:57:35 -0600

    Ready for Day 01, Part 01 to be solved

--- day-01/src/test/groovy/Day01Spec.groovy
+++ day-01/src/test/groovy/Day01Spec.groovy
@@ -3,91 +3,16 @@ import spock.lang.Specification
  * https://adventofcode.com/2025/day/1
  */
 class Safe {
-    int currentValue = 50
-    int zeroCount = 0
-
-    int turnDial(String instruction) {
-        def direction = instruction[0]
-        def amount = instruction.substring(1) as int
-
-        if (direction == 'L') {
-            currentValue -= amount
-        } else {
-            currentValue += amount
-        }
-
-        currentValue %= 100
-        if (currentValue < 0) currentValue += 100
-
-        if (currentValue == 0) {
-            zeroCount++
-        }
-
-        return currentValue
-    }
+    // TODO: Implement this
 }
 
 class Day01Spec extends Specification {
     def safe = new Safe()
 
-    def "Test turnDial"() {
-        expect:
-        safe.turnDial(instruction) == expectedValue
-        safe.zeroCount == zeroCount
-
-        where:
-        instruction || expectedValue || zeroCount
-        'L0'        || 50            || 0
-        'R0'        || 50            || 0
-        'L1'        || 49            || 0
-        'R1'        || 51            || 0
-        'L50'       || 0             || 1
-        'R50'       || 0             || 1
-        'L51'       || 99            || 0
-        'R51'       || 1             || 0
-    }
-
-    def "real password test"() {
-        when:
-        safe.turnDial('L50') // get to 0
-        then:
-        safe.zeroCount == 1
-        when:
-        safe.turnDial('R1')
-        then:
-        safe.zeroCount == 1
-        when:
-        safe.turnDial('L1')
-        then:
-        safe.zeroCount == 2
-        when:
-        safe.turnDial('L1')
-        then:
-        safe.zeroCount == 2
-        when:
-        safe.turnDial('R1')
-        then:
-        safe.zeroCount == 3
-
-    }
-
-    def "crack the password"() {
-        given:
-        def resource = getClass().getResource('/input.txt')
-        assert resource != null : "Could not find input.txt"
-        def file = new File(resource.toURI())
-
-        when:
-        file.eachLine { line ->
-            if (line.trim()) {
-                def val = line.trim()
-                def newValue = safe.turnDial(val)
-                println "Turn dial: $val -> $newValue"
-            }
-        }
-        println "Zero Count: ${safe.zeroCount}"
-
-        then:
-        safe.currentValue != null
-    }
+    // TODO: Write tests to solve the Day 01, Part 1 puzzle in README
+    //      Use the input.txt file for the test data
+    //      Create one test named "Solve" that solves the puzzle
+    //      The Solve might need to `println` the answer
+    //      Create additional helper methods and tests as needed
+    
 }
