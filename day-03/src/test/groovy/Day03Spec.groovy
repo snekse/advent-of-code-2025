1266669aecccb9a2fab4c322ad06ae62b20e6031
Derek Eskens <snekse@gmail.com>
2025-12-06 00:32:50 -0600

    Day 03, Part 2 - AI Agent solution

--- day-03/src/test/groovy/Day03Spec.groovy
+++ day-03/src/test/groovy/Day03Spec.groovy
@@ -4,24 +4,39 @@ import spock.lang.Specification
  * https://adventofcode.com/2025/day/3
  */
 class Day03Spec extends Specification {
-    def "Solve"() {
+    def "Solve Part 1"() {
         setup:
         def inputFile = new File("src/test/resources/input.txt")
         def input = inputFile.readLines()
 
         when:
         def totalJoltage = input.sum { line ->
-            calculateMaxJoltage(line)
+            calculateMaxJoltage(line, 2)
         }
 
         then:
-        println "Total output joltage: $totalJoltage"
+        println "Part 1 Total output joltage: $totalJoltage"
         totalJoltage != 0
     }
 
-    def "calculateMaxJoltage correctly identifies max joltage"() {
+    def "Solve Part 2"() {
+        setup:
+        def inputFile = new File("src/test/resources/input.txt")
+        def input = inputFile.readLines()
+
+        when:
+        def totalJoltage = input.sum { line ->
+            calculateMaxJoltage(line, 12)
+        }
+
+        then:
+        println "Part 2 New total output joltage: $totalJoltage"
+        totalJoltage == 167523425665348
+    }
+
+    def "calculateMaxJoltage correctly identifies max joltage for length 2"() {
         expect:
-        calculateMaxJoltage(bank) == expectedMax
+        calculateMaxJoltage(bank, 2) == expectedMax
 
         where:
         bank                  | expectedMax
@@ -29,22 +44,49 @@ class Day03Spec extends Specification {
         "811111111111119"     | 89
         "234234234234278"     | 78
         "818181911112111"     | 92
-        "12345"               | 45 // Example from my own head: 4 then 5 is max possible (wait, 5 is at index 4, 4 is at index 3. 45 is valid. 54 is not) incorrect logic check?
-                                   // "12345": pairs: 12, 13, 14, 15, 23, 24, 25, 34, 35, 45. Max is 45. Correct.
+        "12345"               | 45
     }
 
-    int calculateMaxJoltage(String bank) {
-        int maxJoltage = 0
-        for (int i = 0; i < bank.length() - 1; i++) {
-            for (int j = i + 1; j < bank.length(); j++) {
-                int firstDigit = Character.getNumericValue(bank.charAt(i))
-                int secondDigit = Character.getNumericValue(bank.charAt(j))
-                int joltage = firstDigit * 10 + secondDigit
-                if (joltage > maxJoltage) {
-                    maxJoltage = joltage
+    def "calculateMaxJoltage correctly identifies max joltage for length 12"() {
+        expect:
+        calculateMaxJoltage(bank, 12) == expectedMax
+
+        where:
+        bank                  | expectedMax
+        "987654321111111"     | 987654321111L
+        "811111111111119"     | 811111111119L
+        "234234234234278"     | 434234234278L
+        "818181911112111"     | 888911112111L
+    }
+
+    long calculateMaxJoltage(String bank, int length) {
+        StringBuilder maxJoltageStr = new StringBuilder()
+        int currentSearchIndex = 0
+
+        for (int i = 0; i < length; i++) {
+            // Find the largest digit in the valid remaining range
+            // We need to pick (length - i) more digits (including this one)
+            // So we must stop searching at: bank.length() - (length - i)
+            // Example: length 2, i=0. Need 2 total. Remaining 2. Stop at len - 2.
+            // i=1. Need 1. Remaining 1. Stop at len - 1 (last char).
+            
+            char maxDigit = '/' // '0' - 1
+            int foundAtIndex = -1
+            int searchEnd = bank.length() - (length - i)
+
+            for (int j = currentSearchIndex; j <= searchEnd; j++) {
+                char currentDigit = bank.charAt(j)
+                if (currentDigit > maxDigit) {
+                    maxDigit = currentDigit
+                    foundAtIndex = j
                 }
+                if (maxDigit == '9') break // Optimization: can't get better than 9
             }
+
+            maxJoltageStr.append(maxDigit)
+            currentSearchIndex = foundAtIndex + 1
         }
-        return maxJoltage
+        
+        return Long.parseLong(maxJoltageStr.toString())
     }
 }
